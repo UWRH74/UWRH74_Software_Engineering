f7c14f2b4fa6408fbf2a553e78c382a6b311f667
UWRH74 <UWRH74@SZA0030897>
2022-11-30 14:55:55 +0100

    2. csatlakozás az adatbázishoz

---
+++ UWRH74_gyak9/Controllers/BoatController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UWRH74_gyak9.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            Models.HajosContext context = new();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
