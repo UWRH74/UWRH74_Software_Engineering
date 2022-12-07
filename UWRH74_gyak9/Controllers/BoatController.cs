2ef7b6a4cbafcb7ec03321620dd0c9cff059522a
UWRH74 <UWRH74@SZA0030897>
2022-12-07 15:15:43 +0100

    ez talán működik, de valszeg nem

--- UWRH74_gyak9/Controllers/BoatController.cs
+++ UWRH74_gyak9/Controllers/BoatController.cs
@@ -16,5 +16,30 @@ namespace UWRH74_gyak9.Controllers
 
             return Ok(kérdések);
         }
+
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            Models.HajosContext context = new Models.HajosContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
+
+        [HttpGet]
+        [Route("questions/count")]
+        public int M4() //Tetszőleges metódusnév
+        {
+            Models.HajosContext context = new();
+            int kérdésekSzáma = context.Questions.Count();
+
+            return kérdésekSzáma;
+        }
+
     }
 }
---
+++ UWRH74_gyak9/wwwroot/main.js
@@ -0,0 +1,93 @@
+﻿//fetch('/questions/4')
+//    .then(response => response.json())
+//    .then(data => kérdésMegjelenítés(data));
+
+var jóVálasz;
+var questionId = 4;
+
+var hotList = [];            
+var questionsInHotList = 3; 
+var displayedQuestion;      
+var numberOfQuestions;      
+var nextQuestion = 1;  
+
+window.onload = function (e) {
+    console.log("Oldal betöltve...");
+    init()
+}
+
+
+function init() {
+    for (var i = 0; i < questionsInHotList; i++) {
+        let q = {
+            question: {},
+            goodAnswers: 0
+        }
+        hotList[i] = q;
+    }
+
+    for (var i = 0; i < questionsInHotList; i++) {
+        kérdésBetöltés(nextQuestion, i);
+        nextQuestion++;
+    }
+}
+
+function kérdésBetöltés(questionNumber, destination) {
+    fetch(`/questions/${questionNumber}`)
+        .then(
+            result => {
+                if (!result.ok) {
+                    console.error(`Hibás letöltés: ${response.status}`)
+                }
+                else {
+                    return result.json()
+                }
+            }
+        )
+        .then(
+            q => {
+                hotList[destination].question = q;
+                hotList[destination].goodAnswers = 0;
+                console.log(`A ${questionNumber}. kérdés letöltve a hot list ${destination}. helyére`)
+                if (displayedQuestion == undefined && destination == 0) {
+                    displayedQuestion = 0;
+                    kérdésMegjelenítés(q);
+                }
+            }
+        );
+} 
+
+
+function kérdésMegjelenítés() {
+
+    let kérdés = hotList[displayedQuestion].question;   
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    jóVálasz = kérdés.correctAnswer;
+    if (kérdés.image) {
+        document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+        document.getElementById("kép").classList.remove("rejtett")
+    }
+    else {
+        document.getElementById("kép").classList.add("rejtett")
+    }
+    document.getElementById("válasz1").classList.remove("jó", "rossz");
+    document.getElementById("válasz2").classList.remove("jó", "rossz");
+    document.getElementById("válasz3").classList.remove("jó", "rossz");
+}
+
+function előre() {
+    displayedQuestion++;
+    if (displayedQuestion == questionsInHotList) displayedQuestion = 0;
+    kérdésMegjelenítés();
+}
+
+function vissza() {
+    displayedQuestion--;
+    if (displayedQuestion == questionsInHotList) displayedQuestion = 0;
+    kérdésMegjelenítés();
+}
+
